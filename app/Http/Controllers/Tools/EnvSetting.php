10fd18a6bd831d0c01d354e8abed2a5b1cce6bcb
Hugh Voo <bingyuuu@163.com>
2017-03-05 21:42:45 +0800

    env file

---
+++ app/Http/Controllers/Tools/EnvSetting.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: VULCANTSeries
+ * Date: 2017/3/5
+ * Time: 21:33
+ */
+
+namespace App\Http\Controllers\Tools;
+
+class EnvSetting
+{
+
+    const environmentFile = '.env.test';
+}
